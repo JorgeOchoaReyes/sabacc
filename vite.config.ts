916f9f3b45441c943624011d53c4f91f9eba21d2
Jorge Raul Ochoa-Reyes <jorgeochoareyes@gmail.com>
2026-03-11 15:21:04 -0700

    test

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,10 @@ export default defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
-    nitro(),
+    nitro({
+      rollupConfig: {
+        external: ['firebase-admin'],
+      }
+    }),
   ]
 })  
